e78324f54aac6ba38b8b22d56e34de14bc30a610
Mark Ulmer <65735904+mark-ulmer-exa@users.noreply.github.com>
2026-07-06 17:43:07 -0400

    Add script to create JIRA issues via API
    This script creates an issue in JIRA using the JIRA REST API. It includes authentication and payload setup for the issue details.

---
+++ Automation/JIRA/Create_Issue_JIRA.py
@@ -0,0 +1,61 @@
+import requests
+import wmill
+from requests.auth import HTTPBasicAuth
+
+def main():
+    # URL for JIRA APIs
+    base_url = wmill.get_variable("f/exabeam/JIRA/JIRA/server_url")
+    url = f"{base_url}/rest/api/3/issue"
+
+    # Information for authentication
+    username = wmill.get_variable("f/exabeam/JIRA/JIRA/username")
+    api_token = wmill.get_variable("f/exabeam/JIRA/JIRA/api_key")
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "fields": {
+            "project": {
+                "key": "KAN"
+            },
+            "summary": "Here is a case from Exabeam",
+            "description": {
+                "type": "doc",
+                "version": 1,
+                "content": [
+                    {
+                        "type": "paragraph",
+                        "content": [
+                            {
+                                "type": "text",
+                                "text": "Case created from Exabeam details.."
+                            }
+                        ]
+                    }
+                ]
+            },
+            "issuetype": {
+                "name": "Task"
+            }
+        }
+    }
+
+    # Envoi de la requête POST
+    response = requests.post(
+        url,
+        headers=headers,
+        json=payload,
+        auth=HTTPBasicAuth(username, api_token)
+    )
+
+    # Verify the reponse
+    if response.status_code == 201:
+        print("Issue created successfully!")
+        print("Issue reference:", response.json()["key"])
+    else:
+        print("Problem creating issue in JIRA.")
+        print("Response Status:", response.status_code)
+        print("Reponse :", response.text)
+
